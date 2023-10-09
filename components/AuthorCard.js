ad70bd1e3e0a5e4dfc627235b10f1d7672b2ba1d
Keana Cobarde <keanacobarde@gmail.com>
2023-10-08 21:34:00 -0500

    building the rest of the AuthorCard functionality

--- components/AuthorCard.js
+++ components/AuthorCard.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import Button from 'react-bootstrap/Button';
+import Link from 'next/link';
+import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-// import Link from 'next/link';
-// import { deleteSingleAuthor } from '../api/authorData';
+import { deleteSingleAuthor } from '../api/authorData';
 
 export default function AuthorCard({ authorObj, onUpdate }) {
+  const deleteThisAuthor = () => {
+    if (window.confirm(`Delete ${authorObj.first_name} ${authorObj.last_name}?`)) {
+      deleteSingleAuthor(authorObj.firebaseKey).then(() => onUpdate);
+    }
+  };
+
   return (
     <Card style={{ width: '18rem', margin: '10px' }}>
       <Card.Body>
         <Card.Title>{authorObj.first_name} {authorObj.last_name}</Card.Title>
+        <Link href={`/author/${authorObj.firebaseKey}`} passHref>
+          <Button variant="primary" className="m-2">VIEW</Button>
+        </Link>
+        {/* DYNAMIC LINK TO EDIT THE BOOK DETAILS  */}
+        <Link href={`/author/edit/${authorObj.firebaseKey}`} passHref>
+          <Button variant="info">EDIT</Button>
+        </Link>
+        <Button variant="danger" onClick={deleteThisAuthor} className="m-2">
+          DELETE
+        </Button>
       </Card.Body>
     </Card>
   );
