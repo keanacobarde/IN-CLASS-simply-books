32e49c8395d9cb660ba343b328f40a06e63af511
Keana Cobarde <keanacobarde@gmail.com>
2023-10-15 16:35:13 -0500

    getting order details functionality sorted out and reduce for price total implemented

--- api/mergedData.js
+++ api/mergedData.js
@@ -1,6 +1,6 @@
 import { getAuthorBooks, getSingleAuthor, deleteSingleAuthor } from './authorData';
 import { getSingleBook, deleteBook, getBooks } from './bookData';
-import { getOrderBooks } from './orderBookData';
+import { getOrderBooks, getSingleBookOrder } from './orderBookData';
 
 const viewBookDetails = (bookFirebaseKey) => new Promise((resolve, reject) => {
   getSingleBook(bookFirebaseKey)
@@ -30,28 +30,40 @@ const deleteAuthorBooks = (authorId) => new Promise((resolve, reject) => {
   }).catch((error) => reject(error));
 });
 
-// const getOrderDetails = async (orderId) => {
-//   const order = getSingleOrder(orderId);
+const getOrderDetails = async (orderId) => {
+  const order = await getSingleBookOrder(orderId);
 
-//   const ge
-// }
+  const allOrderBooks = await getOrderBooks(orderId);
+
+  const getSingleBooks = await allOrderBooks.map((book) => getSingleBook(book.bookId));
+
+  const orderBooks = await Promise.all(getSingleBooks);
+
+  return { ...order, orderBooks };
+};
 
 // getting book orders and filtering out books that aren't in the order
 
-const getBooksNotInOrder = async (uid, orderId) => {
+const getBooksNotInTheOrder = async (uid, orderId) => {
+  // GET ALL THE BOOKS
   const allBooks = await getBooks(uid);
 
+  // GET ALL THE ORDERBOOKS RELATES TO THE ORDER
   const orderBooks = await getOrderBooks(orderId);
 
-  const bookPromises = await orderBooks.map((orderBook) => getSingleBook(orderBook.orderId));
+  // GET THE BOOKS FOUND IN THE ORDER BOOKS, RETURNS AN ARRAY OF PROMISES
+  const bookPromises = await orderBooks.map((orderBook) => getSingleBook(orderBook.bookId));
 
+  // MOST USE PROMISE.ALL() TO RETURN EACH BOOK OBJECT
   const books = await Promise.all(bookPromises);
 
+  // FILTER AND COMPARE THE TWO ARRAYS OF ALL BOOKS AND ALL ORDERBOOKS
   const filterBooks = await allBooks.filter((obj) => !books.some((e) => e.firebaseKey === obj.firebaseKey));
 
+  // ONLY RETURN THE BOOKS NOT RELATED TO ORDER
   return filterBooks;
 };
 
 export {
-  viewBookDetails, viewAuthorDetails, deleteAuthorBooks, getBooksNotInOrder,
+  viewBookDetails, viewAuthorDetails, deleteAuthorBooks, getBooksNotInTheOrder, getOrderDetails,
 };
--- pages/order/[firebaseKey].js
+++ pages/order/[firebaseKey].js
@@ -1,10 +1,10 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import { Card, Button, CardBody } from 'react-bootstrap';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useAuth } from '../../utils/context/authContext';
-import { getBooksNotInOrder } from '../../api/mergedData';
+import { getBooksNotInTheOrder, getOrderDetails } from '../../api/mergedData';
+import { updateOrderBook, createOrderBook } from '../../api/orderBookData';
 
 export default function ViewBookOrderDetails() {
   const { user } = useAuth();
@@ -15,29 +15,56 @@ export default function ViewBookOrderDetails() {
   const { firebaseKey } = router.query;
 
   useEffect(() => {
-    getBooksNotInOrder(user.uid, firebaseKey).then(setBooksNotInOrder);
-  }, []);
+    getBooksNotInTheOrder(user.uid, firebaseKey).then(setBooksNotInOrder);
+    getOrderDetails(firebaseKey).then(setBookDetails);
+  }, [bookDetails.orderBooks]);
 
-  console.warn(bookDetails, setBookDetails, booksNotInOrder);
+  const handlingBookOrder = (bookFirebaseKey) => {
+    // when the add to cart button is pressed and entry needs to be added in orderBooks table. this entry should include the following: the orderId (firebaseKey), the book fbk, and it should have an fbk of its own.
+    const payload = {
+      orderId: firebaseKey,
+      bookId: bookFirebaseKey,
+    };
+
+    createOrderBook(payload).then(({ name }) => {
+      const patchPayload = { firebaseKey: name };
+      updateOrderBook(patchPayload).then(router.push(`/order/${firebaseKey}`));
+    });
+  };
+
+  console.warn(bookDetails);
 
   return (
-    // books not yet in order
-    <div>
-      <h6 className="text-white mt-5"> ADD BOOKS TO CART </h6>
-      <div className="mb-3 text-white mt-5" style={{ display: 'flex' }}>
-        {booksNotInOrder.map((book) => (
-          <Card style={{ width: '18rem', margin: '10px' }}>
-            <Card.Img variant="top" src={book.image} alt={book.title} style={{ height: '400px' }} />
-            <CardBody>
-              <Card.Title>{book.title}</Card.Title>
-              <p className="card-text bold">{book.sale && <span>SALE<br /></span> } ${book.price}</p>
-              <Link href="/" passHref>
-                <Button variant="success" className="m-2">ADD TO CART</Button>
-              </Link>
-            </CardBody>
-          </Card>
-        ))}
+    <>
+      <div>
+        <h6 className="text-white mt-5"> BOOKS IN CART </h6>
+        <h6 className="text-white mt-5"> Total: {bookDetails.orderBooks?.reduce((acc, curr) => Number(acc) + Number(curr.price), 0).toFixed(2)}</h6>
+        <div className="mb-3 text-white mt-5" style={{ display: 'flex' }}>
+          {bookDetails.orderBooks?.map((book) => (
+            <Card style={{ width: '18rem', margin: '10px' }}>
+              <Card.Img variant="top" src={book.image} alt={book.title} style={{ height: '400px' }} />
+              <CardBody>
+                <Card.Title>{book.title}</Card.Title>
+              </CardBody>
+            </Card>
+          ))}
+        </div>
       </div>
-    </div>
+      <div>
+        <h6 className="text-white mt-5"> ADD BOOKS TO CART </h6>
+        <div className="mb-3 text-white mt-5" style={{ display: 'flex' }}>
+          {booksNotInOrder.map((book) => (
+            <Card style={{ width: '18rem', margin: '10px' }}>
+              <Card.Img variant="top" src={book.image} alt={book.title} style={{ height: '400px' }} />
+              <CardBody>
+                <Card.Title>{book.title}</Card.Title>
+                <p className="card-text bold">{book.sale && <span>SALE<br /></span> } ${book.price}</p>
+                <Button variant="success" className="m-2" onClick={() => handlingBookOrder(book.firebaseKey)}>ADD TO CART</Button>
+              </CardBody>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
